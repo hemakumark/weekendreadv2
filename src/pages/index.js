305f24b151eb5c168349e0bed5403a8d8ceb06d6
Hemakumar Kakaraparti <hemkakar@WKMIN4716841.global.publicisgroupe.net>
2018-06-27 14:48:38 +0530

    Delivering changes done for firebase deployment

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/articles/">Go to articles </Link>
     Modified index page to see the changes.
   </div>
 )
